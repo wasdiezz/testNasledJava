d3e3c43cfdf7b449e61ae19e935d0fdd47124db4
wasdiezz <vkavelmakher@mail.ru>
2024-02-05 17:00:36 +0300

    Initial commit

---
+++ src/CarType.java
@@ -0,0 +1,16 @@
+
+public enum CarType {
+    Sport("sport"),
+    Family("family"),
+    Eco("eco");
+
+    private String value;
+
+    CarType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
